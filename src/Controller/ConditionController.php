21890551f7b6729b87356c5177eac3c7de1a8c13
Mastrakas <71260092+Mastrakas@users.noreply.github.com>
2020-11-19 11:00:44 +0100

    lien path twig

--- src/Controller/ConditionController.php
+++ src/Controller/ConditionController.php
@@ -68,30 +68,35 @@ class ConditionController extends AbstractController {
     public function agents() {
         $agents = [
             1 => [
+                "id" => 1,
                 "lastName" => "Robert",
                 "firstName" => "David",
                 "age" => 30,
                 "published" => true
             ],
             2 => [
+                "id" => 2,
                 "lastName" => "Labaste",
                 "firstName" => "Denis",
                 "age" => 29,
                 "published" => true
             ],
             3 => [
+                "id" => 3,
                 "lastName" => "Rozand",
                 "firstName" => "Mathieu",
                 "age" => 31,
                 "published" => false
             ],
             4 => [
+                "id" => 4,
                 "lastName" => "Despert",
                 "firstName" => "Yoann",
                 "age" => 33,
                 "published" => true
             ],
             5 => [
+                "id" => 5,
                 "lastName" => "Dorignac",
                 "firstName" => "Loic",
                 "age" => 34,
@@ -102,6 +107,55 @@ class ConditionController extends AbstractController {
         return $this->render('agents.html.twig',
         [
             'agents' => $agents
+        ]);
+    }
+
+    /**
+     * @Route ("/agent/{id}", name="agent_show")
+     */
+    public function agentShow($id) {
+        $agents = [
+            1 => [
+                "id" => 1,
+                "lastName" => "Robert",
+                "firstName" => "David",
+                "age" => 30,
+                "published" => true
+            ],
+            2 => [
+                "id" => 2,
+                "lastName" => "Labaste",
+                "firstName" => "Denis",
+                "age" => 29,
+                "published" => true
+            ],
+            3 => [
+                "id" => 3,
+                "lastName" => "Rozand",
+                "firstName" => "Mathieu",
+                "age" => 31,
+                "published" => false
+            ],
+            4 => [
+                "id" => 4,
+                "lastName" => "Despert",
+                "firstName" => "Yoann",
+                "age" => 33,
+                "published" => true
+            ],
+            5 => [
+                "id" => 5,
+                "lastName" => "Dorignac",
+                "firstName" => "Loic",
+                "age" => 34,
+                "published" => false
+            ]
+        ];
+
+        $agent = $agents[$id];
+
+        return $this->render('agent.html.twig',[
+            'agent' => $agent
         ]);
     }
 }
